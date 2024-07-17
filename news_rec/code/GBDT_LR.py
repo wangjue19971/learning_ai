e06fd33ea7444fc88aabe520dded0d0461c3f049
wangjue19971 <449039270@qq.com>
2024-07-17 08:48:43 +0800

    concat recall and sort

--- news_rec/code/GBDT_LR.py
+++ news_rec/code/GBDT_LR.py
@@ -1,5 +1,6 @@
 import numpy as np
 import lightgbm as lgb
+import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
@@ -9,16 +10,18 @@ class GBDT_LR:
         self.gbdt_model = None
         self.lr_model = None
 
-    def train_gbdt_lr(self, interactions):
+    def train_gbdt_lr(self, interactions, index2id, id2index):
         # 生成训练数据
         X = []
         y = []
         for (user_id, item_id, label) in interactions:
-            # 将ID值缩放到较小的范围内
-            feature = np.array([user_id % 10000, item_id % 10000], dtype=np.int32)
-            X.append(feature)
-            y.append(int(label))
-
+            if user_id in id2index and item_id in id2index:
+                user_index = id2index[user_id]
+                item_index = id2index[item_id]
+                feature = [user_index, item_index]
+                X.append(feature)
+                y.append(label)
+        
         X = np.array(X)
         y = np.array(y)
 
@@ -26,38 +29,47 @@ class GBDT_LR:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         # 训练GBDT模型
-        train_data = lgb.Dataset(X_train, label=y_train)
-        self.gbdt_model = lgb.train({}, train_data)
+        self.gbdt_model = lgb.LGBMClassifier()
+        self.gbdt_model.fit(X_train, y_train)
 
         # 生成GBDT模型的叶节点索引特征
         gbdt_train_features = self.gbdt_model.predict(X_train, pred_leaf=True)
         gbdt_test_features = self.gbdt_model.predict(X_test, pred_leaf=True)
 
-        # 将叶节点索引特征展平为2D数组
-        gbdt_train_features = gbdt_train_features.reshape(gbdt_train_features.shape[0], -1)
-        gbdt_test_features = gbdt_test_features.reshape(gbdt_test_features.shape[0], -1)
-
         # 训练LR模型
-        self.lr_model = LogisticRegression()
+        self.lr_model = LogisticRegression(max_iter=1000)
         self.lr_model.fit(gbdt_train_features, y_train)
 
         # 测试LR模型
         lr_pred = self.lr_model.predict_proba(gbdt_test_features)[:, 1]
         print("测试集AUC: ", roc_auc_score(y_test, lr_pred))
 
-    def predict(self, user_id, item_id):
+    def predict(self, user_id, item_id, id2index):
         # 确保GBDT模型已经被训练
         if self.gbdt_model is None:
             print("GBDT模型尚未训练，请先调用train_gbdt_lr方法进行训练。")
             return 0.0
 
-        feature = np.array([user_id % 10000, item_id % 10000], dtype=np.int32).reshape(1, -1)
-        gbdt_feature = self.gbdt_model.predict(feature, pred_leaf=True).reshape(1, -1)
-        lr_pred = self.lr_model.predict_proba(gbdt_feature)[:, 1]
-        return lr_pred[0]
+        if user_id in id2index and item_id in id2index:
+            user_index = id2index[user_id]
+            item_index = id2index[item_id]
+            feature = np.array([[user_index, item_index]])
+            gbdt_feature = self.gbdt_model.predict(feature, pred_leaf=True)
+            lr_pred = self.lr_model.predict_proba(gbdt_feature)[:, 1]
+            return lr_pred[0]
+        return 0.0
 
 if __name__ == "__main__":
     gbdt_lr = GBDT_LR()
+    
+    # 加载索引和ID的映射关系
+    index2id = {}
+    id2index = {}
+    with open("news_rec/data/index") as lines:
+        for line in lines:
+            line = line.strip().split("\t")
+            index2id[int(line[1])] = line[0]
+            id2index[line[0]] = int(line[1])
 
     # 打开文件
     file_path = "news_rec/data/mini_news.dat"
@@ -69,9 +81,9 @@ if __name__ == "__main__":
     # 遍历每一行，提取user, item, ctr，并将它们组合成元组
     for line in lines:
         line = line.strip().split("\t")
-        user_id = int(line[1])
-        item_id = int(line[2])
-        label = line[3]
+        user_id = line[1]
+        item_id = line[2]
+        label = int(line[3])
         interactions.append((user_id, item_id, label))
 
-    gbdt_lr.train_gbdt_lr(interactions)
+    gbdt_lr.train_gbdt_lr(interactions, index2id, id2index)
--- news_rec/code/RecallSort.py
+++ news_rec/code/RecallSort.py
@@ -17,27 +17,22 @@ class RecallAndSort:
 
         # 加载GBDT+LR模型
         self.gbdt_lr = GBDT_LR()
-        self.gbdt_lr.train_gbdt_lr(self.load_interactions())
+        self.gbdt_lr.train_gbdt_lr(self.load_interactions(), self.index2id, self.id2index)
 
     def load_interactions(self):
-        # 打开文件
+        # 加载交互数据
+        interactions = []
         file_path = "news_rec/data/mini_news.dat"
         lines = open(file_path, encoding="utf8").readlines()
-
-        # 初始化一个空列表来存储元组
-        interactions = []
-
-        # 遍历每一行，提取user, item, ctr，并将它们组合成元组
         for line in lines:
             line = line.strip().split("\t")
-            user_id = int(line[1])
-            item_id = int(line[2])
+            user_id = line[1]
+            item_id = line[2]
             label = int(line[3])
             interactions.append((user_id, item_id, label))
-
         return interactions
 
-    def recall(self, user_id, item_emb_dic):
+    def recall(self, user_id, user_emb_dic, item_emb_dic):
         # 召回逻辑
         item_id = []
         item_emb_numpy = []
@@ -49,8 +44,8 @@ class RecallAndSort:
         tree = BallTree(np.squeeze(np.array(item_emb_numpy)), leaf_size=10)
         recall_results = {}
         # 对指定用户嵌入向量进行查询，找到最近的k个物品
-        if user_id in item_emb_dic:
-            v = item_emb_dic[user_id]
+        if user_id in user_emb_dic:
+            v = user_emb_dic[user_id]
             n_points = tree.data.shape[0]  # 获取树中点的数量
             num_neighbors = min(20, n_points)  # 确保k不超过点的数量
             dist, ind = tree.query(v.reshape(1, -1), k=num_neighbors)
@@ -58,28 +53,58 @@ class RecallAndSort:
         return recall_results
 
     def sort(self, user_id, recall_result):
-        # 对召回结果进行排序
-        sorted_result = {}
-        for item in recall_result[user_id]:
-            sorted_result[item] = self.gbdt_lr.predict(user_id, item)
-
-        # 根据预测分数进行排序
-        sorted_result = dict(sorted(sorted_result.items(), key=lambda x: x[1], reverse=True))
-        return sorted_result
-
+        result = {}
+        for item_id in recall_result[user_id]:
+            score = self.gbdt_lr.predict(user_id, item_id, self.id2index)
+            result[item_id] = score
+        return result
+    
 if __name__ == "__main__":
     ras = RecallAndSort()
-
+    user_id = "2380244356815169921"  # 指定用户ID
+    
     # 加载用户和物品嵌入向量
+    user_emb_dic = {}
+    item_emb_dic = {}
     with open('news_rec/data/user_emb_dic.pkl', 'rb') as user_file:
         user_emb_dic = pickle.load(user_file)
     with open('news_rec/data/item_emb_dic.pkl', 'rb') as item_file:
         item_emb_dic = pickle.load(item_file)
 
-    user_id = '2380244356815169921'  # 指定用户ID
     # 使用加载的用户和物品嵌入向量进行召回
-    recall_result = ras.recall(user_id, item_emb_dic)
+    recall_result = ras.recall(user_id, user_emb_dic, item_emb_dic)
     print(f"召回结果: {recall_result[user_id]}")  # 修改这里以打印指定用户的召回结果
+
     # 对召回结果进行排序
     sort_result = ras.sort(user_id, recall_result)
     print(f"排序结果: {sort_result}")
+    
+# 测试集AUC:  0.6542653950341825
+# 召回结果: ['4106279991666292577', '3304821928338655444', '16529623828211313387', 
+#       '10265030586183854448', '6789838657778527135', '11770742125479994390', 
+#       '3054423453900014545', '4080452504006450662', '12088043601288016710', 
+#       '12203214915075060334', '4920758881479676410', '1719696861020491404', 
+#       '8993157435258500152', '17371650719194954318', '5019091515877081211', 
+#       '2996524628436276241', '16294764181418169402', '2545858582159222152', 
+#       '6300686928420967038', '1000356838604552172']
+
+# 排序结果: {'4106279991666292577': 0.40465389731811896, 
+# '3304821928338655444': 0.3818071339537923, 
+# '16529623828211313387': 0.3523366214190019, 
+# '10265030586183854448': 0.22466121560243568, 
+# '6789838657778527135': 0.22466121560243568, 
+# '11770742125479994390': 0.22466121560243568, 
+# '3054423453900014545': 0.3818071339537923, 
+# '4080452504006450662': 0.34369277483777927, 
+# '12088043601288016710': 0.4698197813065415, 
+# '12203214915075060334': 0.14969070443960095, 
+# '4920758881479676410': 0.4863190265514221, 
+# '1719696861020491404': 0.6139146181645714, 
+# '8993157435258500152': 0.41539336002500676, 
+# '17371650719194954318': 0.4855973599791859, 
+# '5019091515877081211': 0.3592087762948819, 
+# '2996524628436276241': 0.4887125319367385, 
+# '16294764181418169402': 0.3590121516299831, 
+# '2545858582159222152': 0.4945621501954395, 
+# '6300686928420967038': 0.33083249573252704, 
+# '1000356838604552172': 0.22807822598409688}
